f24616ac6a76f48deac6be195cfd786bcdf291f2
MuhammadTalha313 <talhabadar55@gmail.com>
2023-01-05 03:30:06 +0500

    Use case file is updated

--- EyeByeDirect/src/main/AddProduct.java
+++ EyeByeDirect/src/main/AddProduct.java
@@ -4,15 +4,15 @@ import java.sql.*;
 
 public class AddProduct {
 
-	public String addProductInfo(String Name , String Description , String Type , int CategoryID , float Size , float Weight , 
+	public String addProductInfo(int id,String Name , String Description , String Type , int CategoryID , float Size , float Weight , 
 			String Color , int StandardCost , int ListPrice ) {
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con =
 			DriverManager.getConnection("jdbc:mysql://localhost:3306/eyebuydirect","root","");
 			Statement stmt=con.createStatement();
-			stmt.executeUpdate("insert into product(Name , Description , Type , CategoryID , Size , Weight , Color, StandardCost , ListPrice) "
-					+ "values('"+Name+"' , '"+Description+"' , '"+Type+"' , '"+CategoryID+"' , '"+Size+"' , '"+Weight+"' ,  '"+Color+"' "
+			stmt.executeUpdate("insert into product(ProductID,Name , Description , Type , CategoryID , Size , Weight , Color, StandardCost , ListPrice) "
+					+ "values('"+id+"','"+Name+"' , '"+Description+"' , '"+Type+"' , '"+CategoryID+"' , '"+Size+"' , '"+Weight+"' ,  '"+Color+"' "
 							+ ",  '"+StandardCost+"' ,  '"+ListPrice+"');");
 
 		
--- EyeByeDirect/src/main/ReceiveOrder.java
+++ EyeByeDirect/src/main/ReceiveOrder.java
@@ -1,5 +1,27 @@
 package main;
 
+import java.sql.*;
+
 public class ReceiveOrder {
 
+	public String addOrderInfo(int CustomerID, int Orderqty,int UnitPrice, int productid  ) {
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/eyebuydirect","root","");
+			Statement stmt=con.createStatement();
+			stmt.executeUpdate("insert into orderdetail(CustomerID, OrderQty , UnitPrice , ProductID ) "
+					+ "values('"+CustomerID+"','"+Orderqty+"' , '"+UnitPrice+"' , '"+productid+"');");
+
+		
+			con.close();
+			
+			} catch(Exception e)
+			{
+			System.out.println(e);
+			}
+		
+		return " ";
+	
+	}
 }
